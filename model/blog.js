4669d1926ce4658aa2cf980049e3e2c766254a14
abhii33318 <itsmeabhijithp@gmail.com>
2023-11-08 12:16:56 +0530

    updated

--- model/blog.js
+++ model/blog.js
@@ -12,7 +12,7 @@ const blogSchema = mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
+        // required: true
     },
     category:{
         type: String,
